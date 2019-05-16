7f72bfd5917556d922e1e078def07b387a2f2838
alexthebobo <50594846+alexthebobo@users.noreply.github.com>
2019-05-15 20:57:00 -0400

    Add files via upload

--- submit/populate.py
+++ submit/populate.py
@@ -11,12 +11,18 @@ nonfiction2 = Tome_Rater.create_non_fiction("Computing Machinery and Intelligenc
 novel2 = Tome_Rater.create_novel("The Diamond Age", "Neal Stephenson", 10101010)
 novel3 = Tome_Rater.create_novel("There Will Come Soft Rains", "Ray Bradbury", 10001000)
 
+print("##############################################")
+
 #Create users:
 Tome_Rater.add_user("Alan Turing", "alan@turing.com")
 Tome_Rater.add_user("David Marr", "david@computation.org")
 
+print("##############################################")
+
 #Add a user with three books already read:
 Tome_Rater.add_user("Marvin Minsky", "marvin@mit.edu", user_books=[book1, novel1, nonfiction1])
+
+print("##############################################")
 
 #Add books to a user one by one, with ratings:
 Tome_Rater.add_book_to_user(book1, "alan@turing.com", 1)
@@ -31,12 +37,14 @@ Tome_Rater.add_book_to_user(novel3, "david@computation.org", 4)
 
 
 #Uncomment these to test your functions:
-# Tome_Rater.print_catalog()
-# Tome_Rater.print_users()
+#Tome_Rater.print_catalog()
+#Tome_Rater.print_users()
 
-# print("Most positive user:")
-# print(Tome_Rater.most_positive_user())
-# print("Highest rated book:")
-# print(Tome_Rater.highest_rated_book())
-# print("Most read book:")
-# print(Tome_Rater.most_read_book())
+#print()
+
+#print("Most positive user:")
+#print(Tome_Rater.most_positive_user())
+print("Highest rated book:")
+print(Tome_Rater.highest_rated_book())
+#print("Most read book:")
+#print(Tome_Rater.most_read_book())
